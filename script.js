39a0443b676b7ccbfe68ff5c83ba630064632562
FranGuz <guzmanf92@gmail.com>
2017-11-14 08:55:37 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+
+
+$( "document" ).ready(function() {
+    var type =null
+    $("button").on("click",function(){
+        var choice =$(".choice").val();
+        $.get("https://pokeapi.co/api/v2/pokemon/"+choice+"/").done(function(response){
+            $(".sprite").empty();
+            $(".sprite").removeClass(type);
+            $("h3").remove();
+            type=(response.types[0].type.name)
+            if ((response.types.length===2)){
+                type2="/"+(response.types[1].type.name)
+            }else{
+                type2=""
+            }
+            ID=response.id
+            name=response.name
+            height=response.height/10+" m"
+            weight=response.weight/10+" kg";
+            $(".sprite").append("<img src="+response.sprites.front_default+">");
+            $(".info").append("<h3> ID: "+ID+" <br> Name: "+name+" <br> Height: "+height+" <br> Weight: "+weight+" <br> Type: "+type +type2+" </h3>");
+            $(".sprite").addClass(response.types[0].type.name);
+            console.log(response);
+        });
+});
+});
